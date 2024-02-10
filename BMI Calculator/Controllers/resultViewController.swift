cd766de3c1f1195624bdd16797d475b684f5b954
justerkb <erkebulan.nurbosyn@gmail.com>
2024-02-10 15:25:16 +0600

    the final done project +BMI struct, changes in result view and view contriller classes

---
+++ BMI Calculator/Controllers/resultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  resultViewController.swift
+//  BMI Calculator
+//
+//  Created by Yerkebulan on 03.02.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class resultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BMI Calculator/Controllers/secondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  secondViewController.swift
+//  BMI Calculator
+//
+//  Created by Yerkebulan on 03.02.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
